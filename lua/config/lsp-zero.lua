51fbd78c93f9e2f79f3d3724ef44bd833f4a90ab
Karsten Westra <karstenwestra@gmail.com>
2023-12-18 23:16:22 +0100

    Tweaking LSP-Zero setup.

--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -5,7 +5,6 @@ lsp.preset("recommended")
 local cmp = require("cmp")
 local cmp_action = lsp.cmp_action()
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
 local cmp_mapping = cmp.mapping.preset.insert({
   ["<CR>"] = cmp.mapping.confirm({ select = true }),
   
@@ -17,29 +16,25 @@ local cmp_mapping = cmp.mapping.preset.insert({
 })
 cmp.setup({ mapping = cmp_mapping })
 
-lsp.set_preferences({
-  sign_icons = {}
-})
-
+lsp.set_preferences({ sign_icons = {} })
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp.default_keymaps({ buffer = bufnr })
+  lsp.default_keymaps({ buffer = bufnr, preserve_mappings = false })
   
-  local opts = { buffer = bufnr, remap = false }
+--  local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+--  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+--  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+--  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+--  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+--  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+--  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
-
 lsp.setup()
 
 local lsp_config = require('lspconfig')
