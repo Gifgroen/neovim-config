3e7e6d4b0c37b09596579654d7cdf5b253d3aed5
Karsten Westra <karsten@gpal.nl>
2024-11-04 11:37:36 +0100

    Added basic setup for Lazy plugin management.

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
--- TODO: setup Lazy.
-
 vim.api.nvim_exec('language en_US', true)
+
+vim.g.have_nerd_font = true
+vim.g.mapleader = " "
+
+require('config.lazy')
 
--- plugin/g.lua
+++
@@ -1,9 +0,0 @@
-local g = vim.g
-
--- Because we are nerd :)
-g.have_nerd_font = true
-
--- Leader
-g.mapleader = " "
-g.maplocalleader = " "
-
